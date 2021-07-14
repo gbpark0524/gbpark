9501f9c18ddb184d4fcde5c6def637eca0cb330e
gbpark0524 <gbpark0524@gmail.com>
2021-07-14 17:27:57 +0900

    lucy-xss-servlet-filter 적용

---
+++ src/main/java/kr/pe/gbpark/util/WebConfig.java
@@ -0,0 +1,22 @@
+package kr.pe.gbpark.util;
+
+import com.navercorp.lucy.security.xss.servletfilter.XssEscapeServletFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	//Lucy Xss filter 적용
+	@Bean
+	public FilterRegistrationBean<XssEscapeServletFilter> getFilterRegistrationBean(){
+		FilterRegistrationBean<XssEscapeServletFilter> registrationBean = new FilterRegistrationBean<>();
+		registrationBean.setFilter(new XssEscapeServletFilter());
+		registrationBean.setOrder(1);
+		registrationBean.addUrlPatterns("/*");
+		return registrationBean;
+	}
+
+}
