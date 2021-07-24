3f9ff0b29f0982b8fa8db25c1ae7d3bcbab3ee19
gbpark0524 <gbpark0524@gmail.com>
2021-07-24 22:32:31 +0900

    컨트롤러 로그 추가

--- src/main/java/kr/pe/gbpark/web/common/ErrorController.java
+++ src/main/java/kr/pe/gbpark/web/common/ErrorController.java
@@ -1,13 +1,16 @@
 package kr.pe.gbpark.web.common;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+	private final Logger log = LoggerFactory.getLogger(getClass());
+
 	@RequestMapping("/error")
 	public String error() {
 		return "common/error";
 	}
-
 }
--- src/main/java/kr/pe/gbpark/web/main/MainController.java
+++ src/main/java/kr/pe/gbpark/web/main/MainController.java
@@ -1,10 +1,14 @@
 package kr.pe.gbpark.web.main;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
+	private final Logger log = LoggerFactory.getLogger(getClass());
+
 	@RequestMapping("/")
 	public String rootMain() {
 		return "main/main";
