17205e876a06ddadafc03367c2f871814446577a
gbpark0524 <gbpark0524@gmail.com>
2021-08-13 22:56:37 +0900

    war, mariadb 에 배포할수 있도록 수정

--- src/main/java/kr/pe/gbpark/GbparkApplication.java
+++ src/main/java/kr/pe/gbpark/GbparkApplication.java
@@ -1,8 +1,13 @@
 package kr.pe.gbpark;
 
 import kr.pe.gbpark.util.service.UserUtil;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -12,17 +17,18 @@ import java.util.Optional;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class GbparkApplication {
+public class GbparkApplication extends SpringBootServletInitializer {
 
-	final
+	@Autowired
 	HttpServletRequest request;
-
-	public GbparkApplication(HttpServletRequest request) {
-		this.request = request;
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GbparkApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(GbparkApplication.class);
 	}
 
 	@Bean
